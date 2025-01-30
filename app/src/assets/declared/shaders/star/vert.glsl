75d35b3935aae8550b8c18695a366ff905f841eb
AnHuynh <102804094+Huynhphuantest@users.noreply.github.com>
2025-01-30 10:54:59 +0000

    fix: make star more random

--- app/src/assets/declared/shaders/star/vert.glsl
+++ app/src/assets/declared/shaders/star/vert.glsl
@@ -5,15 +5,16 @@ varying vec3 vColor;
 attribute float size;
 
 // #includes
+
 void main() {
     vColor = color;
-    vec3 REP = vec3(100000.0, 0.0, 100000.0);
+
     vec3 dir = normalize(vec3(
-        pnoise(normalize(color.xyz + position.xzy), REP),
-        pnoise(normalize(color.zyx + position.zxy), REP),
-        pnoise(normalize(color.yxz + position.yzx), REP)
+        random(position.x),
+        random(position.y),
+        random(position.z)
     ));
-    mat4 rotationMatrix = rotation3d(dir, uTime * pnoise(dir, REP) * uSpeed);
+    mat4 rotationMatrix = rotation3d(dir, uTime * uSpeed);
     vec4 newPosition = vec4(position, 1.0) * rotationMatrix;
     vec4 mvPosition = modelViewMatrix * newPosition;
     gl_PointSize = size / -mvPosition.z;
